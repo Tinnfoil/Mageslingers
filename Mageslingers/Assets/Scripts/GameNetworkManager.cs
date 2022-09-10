2b1904b80981d285cbffe657f8b55e2f9297a801
Kenny Doan <kennydoan110999@gmail.com>
2022-09-10 14:31:54 -0700

    Initial Push

---
+++ Mageslingers/Assets/Scripts/GameNetworkManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class GameNetworkManager : NetworkManager
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
