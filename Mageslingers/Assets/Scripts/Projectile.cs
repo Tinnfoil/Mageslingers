ddaf996871ca3f1dc34d1584a57e1797d88c0220
Kenny Doan <kennydoan110999@gmail.com>
2022-11-23 16:20:27 -0800

    Adjustments

--- Mageslingers/Assets/Scripts/Projectile.cs
+++ Mageslingers/Assets/Scripts/Projectile.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-//using Sirenix.OdinInspector;
 
 public class Projectile : NetworkActor
 {
-    [Sirenix.OdinInspector.Required]
     public Hitbox hitbox;
     public GameObject Model;
 
